041c97a948d3f00a8fefa3c51257a2eb73fc7d84
xalc <chao1990li@gmail.com>
2024-11-12 16:02:27 +0800

    update constants

--- libs/constant.js
+++ libs/constant.js
@@ -1,7 +1,6 @@
 export const WEREAD_URL = "https://weread.qq.com"
 export const BOOK_SHELF_URL = 'https://i.weread.qq.com/shelf/sync';
 export const READING_TIMES = "https://i.weread.qq.com/readdata/summary"
-export const we_readerCookies = 'wr_gid=247941411; wr_fp=2314917716; wr_vid=16110108; wr_rt=web%40vaFK7z0HSr~0yMigVjj_AL; wr_localvid=72932e806f5d21c729ca120; wr_name=HuntX; wr_avatar=https%3A%2F%2Fthirdwx.qlogo.cn%2Fmmopen%2Fvi_32%2FG2icRHrAN7qmUGfvxVibFnpzOFZ84E6Ribsiarj5WqSSpzaow36Qt9xFqaM1tQialXQUB3zoc9mCfibGkIRlIMI0m7uA%2F132; wr_gender=1; wr_pf=NaN; _clck=m35xqr|1|fqk|0; wr_skey=C20XgPgO';
 
 // for database
 export const KNOWLEDGE_DB_NAME = 'knowledge';
